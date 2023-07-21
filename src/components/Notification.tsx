cd777401e2e0ec8f70edd9fb070bf21dbf3f6371
SAFWAT <safwathkhan2@gmail.com>
2023-07-21 11:03:17 +0530

    added prettier

---
+++ src/components/Notification.tsx
@@ -0,0 +1,9 @@
+const Notification = () => {
+  return (
+    <div className="flex h-12 cursor-pointer items-center justify-center bg-red-500 px-4 text-center text-sm text-white md:text-base">
+      Free delivery for all orders over $250. Order your food now!
+    </div>
+  );
+};
+
+export default Notification;
